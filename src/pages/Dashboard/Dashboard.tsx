c0446ec24e1f36be77d3901bd4cb52b6c77a5230
Nicoogf <nicolasgfalabella@gmail.com>
2023-10-17 11:28:45 -0300

    Commit 23

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,20 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 const Dashboard = () => {
+
+    const [ product, setProduct ] = useState ({
+        aniboSeries : "" ,
+        character : "" ,
+        gameSeries : "" ,
+        head : "" ,
+        image : "" ,
+        name : "" ,
+        releaseData : "" ,
+        tail : "" ,
+        type : "" ,
+        price : 0
+    })
 
     const navigate = useNavigate() ;
 
@@ -12,62 +25,79 @@ const Dashboard = () => {
         } 
     } , [])
 
+    const handleLogout = () => {
+        localStorage.removeItem("userLogin") ;
+        navigate("/login")
+    }
+
+    const handleChange = ( e : React.ChangeEvent<HTMLInputElement>)  =>{
+         setProduct({
+            ...product,
+            [e.target.name] : e.target.value
+         })
+    }
+
+    const handleSubmit = ( e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault() ;
+        console.log(product)
+    }
+
   return (
     <div className="flex flex-col items-center justify-center bg-gray-800 text-white h-screen gap-4">
         <div className="flex flex-col items-center gap-4">
             <h1 className="text-4xl font-bold text-center">Dashoboard</h1>
-            <button className="text-yellow-800 bg-yellow-500 px-4 py-2 rounded-lg font-bold">  Logout </button>
+            <button onClick={ handleLogout }className="text-yellow-800 bg-yellow-500 px-4 py-2 rounded-lg font-bold">  Logout </button>
         </div>
 
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="flex flex-row justify-between items-center">
                 <label htmlFor="aniboSeries"> Anibo Series </label>
-                <input  className="p-2 rounded-lg text-black"type="text" id="aniboSeries" name="aniboSeries" value={''} onChange={()=>{}} required />
+                <input  className="p-2 rounded-lg text-black"type="text" id="aniboSeries" name="aniboSeries" value={ product.aniboSeries} onChange={handleChange} required />
             </div>
 
             <div className="flex flex-row justify-between items-center">
                 <label htmlFor="character"> Character </label>
-                <input className="p-2 rounded-lg  text-black" type="text" id="character" name ="character"value={''} onChange={()=>{}} required />
+                <input className="p-2 rounded-lg  text-black" type="text" id="character" name ="character"value={ product.character} onChange={handleChange} required />
             </div>
 
             <div className="flex flex-row justify-between items-center">
                 <label htmlFor="gameSeries"> Game Series </label>
-                <input className="p-2 rounded-lg  text-black" type="text" id="gameSeries" name ="gameSeries" value={''} onChange={()=>{}} required />
+                <input className="p-2 rounded-lg  text-black" type="text" id="gameSeries" name ="gameSeries" value={ product.gameSeries} onChange={handleChange} required />
             </div>
 
             <div className="flex flex-row justify-between items-center">
                 <label htmlFor="head"> Head </label>
-                <input className="p-2 rounded-lg  text-black" type="text" id="head" name="head" value={''} onChange={()=>{}} required />
+                <input className="p-2 rounded-lg  text-black" type="text" id="head" name="head" value={ product.head} onChange={handleChange} required />
             </div>
 
             <div className="flex flex-row justify-between items-center">
                 <label htmlFor="image"> Image </label>
-                <input className="p-2 rounded-lg  text-black" type="text" id="image" name="image" value={''} onChange={()=>{}} required />
+                <input className="p-2 rounded-lg  text-black" type="text" id="image" name="image" value={ product.image} onChange={handleChange} required />
             </div>
 
             <div className="flex flex-row justify-between items-center">
                 <label htmlFor="name"> Name </label>
-                <input className="p-2 rounded-lg  text-black" type="text" id="name" name="name" value={''} onChange={()=>{}} required />
+                <input className="p-2 rounded-lg  text-black" type="text" id="name" name="name" value={ product.name } onChange={handleChange} required />
             </div>
 
             <div className="flex flex-row justify-between items-center">
-                <label htmlFor="release"> Fecha de Lanzamiento </label>
-                <input className="p-2 rounded-lg  text-black" type="date" id="release" name="release"value={''} onChange={()=>{}} required />
+                <label htmlFor="releaseData"> Fecha de Lanzamiento </label>
+                <input className="p-2 rounded-lg  text-black" type="date" id="releaseData" name="releaseData"value={ product.releaseData} onChange={handleChange} required />
             </div>
 
             <div className="flex flex-row justify-between items-center">
                 <label htmlFor="tail"> Tail </label>
-                <input className="p-2 rounded-lg  text-black" type="text" id="tail" name="tail "value={''} onChange={()=>{}} required />
+                <input className="p-2 rounded-lg  text-black" type="text" id="tail" name="tail" value={ product.tail} onChange={handleChange} required/>
             </div>
 
             <div className="flex flex-row justify-between items-center">
                 <label htmlFor="type"> Type </label>
-                <input className="p-2 rounded-lg  text-black" type="text" id="type" name="type" value={''} onChange={()=>{}} required/>
+                <input className="p-2 rounded-lg  text-black" type="text" id="type" name="type" value={ product.type} onChange={handleChange} required/>
             </div>
 
             <div className="flex flex-row justify-between items-center">
                 <label htmlFor="price"> Price </label>
-                <input className="p-2 rounded-lg  text-black" type="number" id="price" name="price" value={''} onChange={()=>{}} required />
+                <input className="p-2 rounded-lg  text-black" type="number" id="price" name="price" value={ product.price} onChange={handleChange} required />
             </div>
 
             <div className="flex justify-center items-center">
